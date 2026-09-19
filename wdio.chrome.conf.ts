edc07c013e987ebf7f903c72330f7b6fc3598cc9
jjustgray <jjustgray@gmail.com>
2026-09-19 11:48:01 +0300

    fix gh

---
+++ wdio.chrome.conf.ts
@@ -0,0 +1,17 @@
+import { config as baseConfig } from './wdio.conf.js';
+
+export const config: WebdriverIO.Config = {
+    ...baseConfig,
+    capabilities: [{
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: [
+                '--headless=new',
+                '--no-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--window-size=1920,1080'
+            ]
+        }
+    }]
+};
